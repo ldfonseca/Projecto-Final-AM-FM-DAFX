3b902fe39f34cd367d3d7009751fa2f1812bd8c2
ldfonseca <luisfonsecadias@gmail.com>
2025-06-24 01:30:04 +0100

    Add files via upload

---
+++ build/src/ChorIFer.cpp.d
@@ -0,0 +1,245 @@
+build/src/ChorIFer.cpp.o: src/ChorIFer.cpp src/plugin.hpp \
+ C:/VRack/Rack-SDK/include/rack.hpp C:/VRack/Rack-SDK/include/common.hpp \
+ C:/VRack/Rack-SDK/include/arch.hpp C:/VRack/Rack-SDK/include/logger.hpp \
+ C:/VRack/Rack-SDK/include/math.hpp C:/VRack/Rack-SDK/include/string.hpp \
+ C:/VRack/Rack-SDK/include/system.hpp C:/VRack/Rack-SDK/include/mutex.hpp \
+ C:/VRack/Rack-SDK/include/random.hpp \
+ C:/VRack/Rack-SDK/include/network.hpp \
+ C:/VRack/Rack-SDK/dep/include/jansson.h \
+ C:/VRack/Rack-SDK/dep/include/jansson_config.h \
+ C:/VRack/Rack-SDK/include/asset.hpp \
+ C:/VRack/Rack-SDK/include/window/Window.hpp \
+ C:/VRack/Rack-SDK/include/window/Svg.hpp \
+ C:/VRack/Rack-SDK/dep/include/nanovg.h \
+ C:/VRack/Rack-SDK/dep/include/nanosvg.h \
+ C:/VRack/Rack-SDK/dep/include/GL/glew.h \
+ C:/VRack/Rack-SDK/dep/include/GLFW/glfw3.h \
+ C:/VRack/Rack-SDK/dep/include/nanovg_gl.h \
+ C:/VRack/Rack-SDK/dep/include/nanovg_gl_utils.h \
+ C:/VRack/Rack-SDK/include/context.hpp \
+ C:/VRack/Rack-SDK/include/audio.hpp C:/VRack/Rack-SDK/include/midi.hpp \
+ C:/VRack/Rack-SDK/include/settings.hpp \
+ C:/VRack/Rack-SDK/include/color.hpp \
+ C:/VRack/Rack-SDK/include/helpers.hpp \
+ C:/VRack/Rack-SDK/include/plugin/Model.hpp \
+ C:/VRack/Rack-SDK/include/plugin/Plugin.hpp \
+ C:/VRack/Rack-SDK/include/ui/MenuOverlay.hpp \
+ C:/VRack/Rack-SDK/include/widget/OpaqueWidget.hpp \
+ C:/VRack/Rack-SDK/include/widget/Widget.hpp \
+ C:/VRack/Rack-SDK/include/widget/event.hpp \
+ C:/VRack/Rack-SDK/include/weakptr.hpp \
+ C:/VRack/Rack-SDK/include/ui/common.hpp \
+ C:/VRack/Rack-SDK/dep/include/blendish.h \
+ C:/VRack/Rack-SDK/include/ui/MenuItem.hpp \
+ C:/VRack/Rack-SDK/include/ui/Menu.hpp \
+ C:/VRack/Rack-SDK/include/ui/MenuEntry.hpp \
+ C:/VRack/Rack-SDK/include/ui/MenuLabel.hpp \
+ C:/VRack/Rack-SDK/include/app/PortWidget.hpp \
+ C:/VRack/Rack-SDK/include/app/common.hpp \
+ C:/VRack/Rack-SDK/include/ui/Tooltip.hpp \
+ C:/VRack/Rack-SDK/include/engine/Module.hpp \
+ C:/VRack/Rack-SDK/include/engine/Param.hpp \
+ C:/VRack/Rack-SDK/include/engine/Port.hpp \
+ C:/VRack/Rack-SDK/include/engine/Light.hpp \
+ C:/VRack/Rack-SDK/include/engine/ParamQuantity.hpp \
+ C:/VRack/Rack-SDK/include/Quantity.hpp \
+ C:/VRack/Rack-SDK/include/engine/PortInfo.hpp \
+ C:/VRack/Rack-SDK/include/engine/LightInfo.hpp \
+ C:/VRack/Rack-SDK/include/app/ParamWidget.hpp \
+ C:/VRack/Rack-SDK/include/app/ModuleLightWidget.hpp \
+ C:/VRack/Rack-SDK/include/app/MultiLightWidget.hpp \
+ C:/VRack/Rack-SDK/include/app/LightWidget.hpp \
+ C:/VRack/Rack-SDK/include/widget/TransparentWidget.hpp \
+ C:/VRack/Rack-SDK/include/app/Scene.hpp \
+ C:/VRack/Rack-SDK/include/app/RackScrollWidget.hpp \
+ C:/VRack/Rack-SDK/include/ui/ScrollWidget.hpp \
+ C:/VRack/Rack-SDK/include/ui/Scrollbar.hpp \
+ C:/VRack/Rack-SDK/include/widget/ZoomWidget.hpp \
+ C:/VRack/Rack-SDK/include/app/RackWidget.hpp \
+ C:/VRack/Rack-SDK/include/widget/FramebufferWidget.hpp \
+ C:/VRack/Rack-SDK/include/app/RailWidget.hpp \
+ C:/VRack/Rack-SDK/include/app/ModuleWidget.hpp \
+ C:/VRack/Rack-SDK/include/history.hpp \
+ C:/VRack/Rack-SDK/include/app/CableWidget.hpp \
+ C:/VRack/Rack-SDK/include/engine/Cable.hpp \
+ C:/VRack/Rack-SDK/include/app/SvgPanel.hpp \
+ C:/VRack/Rack-SDK/include/widget/SvgWidget.hpp \
+ C:/VRack/Rack-SDK/include/componentlibrary.hpp \
+ C:/VRack/Rack-SDK/include/app/SvgKnob.hpp \
+ C:/VRack/Rack-SDK/include/app/Knob.hpp \
+ C:/VRack/Rack-SDK/include/widget/TransformWidget.hpp \
+ C:/VRack/Rack-SDK/include/app/CircularShadow.hpp \
+ C:/VRack/Rack-SDK/include/app/SvgSlider.hpp \
+ C:/VRack/Rack-SDK/include/app/SliderKnob.hpp \
+ C:/VRack/Rack-SDK/include/app/SvgPort.hpp \
+ C:/VRack/Rack-SDK/include/app/SvgSwitch.hpp \
+ C:/VRack/Rack-SDK/include/app/Switch.hpp \
+ C:/VRack/Rack-SDK/include/app/SvgScrew.hpp \
+ C:/VRack/Rack-SDK/include/app/AudioDisplay.hpp \
+ C:/VRack/Rack-SDK/include/app/LedDisplay.hpp \
+ C:/VRack/Rack-SDK/include/ui/TextField.hpp \
+ C:/VRack/Rack-SDK/include/app/SvgButton.hpp \
+ C:/VRack/Rack-SDK/include/app/MidiDisplay.hpp \
+ C:/VRack/Rack-SDK/include/widget/OpenGlWidget.hpp \
+ C:/VRack/Rack-SDK/include/ui/List.hpp \
+ C:/VRack/Rack-SDK/include/ui/TooltipOverlay.hpp \
+ C:/VRack/Rack-SDK/include/ui/Slider.hpp \
+ C:/VRack/Rack-SDK/include/ui/ProgressBar.hpp \
+ C:/VRack/Rack-SDK/include/ui/MenuSeparator.hpp \
+ C:/VRack/Rack-SDK/include/ui/Label.hpp \
+ C:/VRack/Rack-SDK/include/ui/SequentialLayout.hpp \
+ C:/VRack/Rack-SDK/include/ui/Button.hpp \
+ C:/VRack/Rack-SDK/include/ui/ChoiceButton.hpp \
+ C:/VRack/Rack-SDK/include/ui/OptionButton.hpp \
+ C:/VRack/Rack-SDK/include/ui/RadioButton.hpp \
+ C:/VRack/Rack-SDK/include/engine/ParamHandle.hpp \
+ C:/VRack/Rack-SDK/include/engine/Engine.hpp \
+ C:/VRack/Rack-SDK/include/plugin.hpp \
+ C:/VRack/Rack-SDK/include/plugin/callbacks.hpp \
+ C:/VRack/Rack-SDK/include/dsp/common.hpp \
+ C:/VRack/Rack-SDK/include/simd/functions.hpp \
+ C:/VRack/Rack-SDK/include/simd/Vector.hpp \
+ C:/VRack/Rack-SDK/include/simd/common.hpp \
+ C:/VRack/Rack-SDK/include/simd/sse_mathfun_extension.h \
+ C:/VRack/Rack-SDK/include/simd/sse_mathfun.h \
+ C:/VRack/Rack-SDK/include/dsp/window.hpp \
+ C:/VRack/Rack-SDK/include/dsp/ode.hpp \
+ C:/VRack/Rack-SDK/include/dsp/minblep.hpp \
+ C:/VRack/Rack-SDK/include/dsp/fft.hpp \
+ C:/VRack/Rack-SDK/dep/include/pffft.h \
+ C:/VRack/Rack-SDK/include/dsp/ringbuffer.hpp \
+ C:/VRack/Rack-SDK/include/dsp/resampler.hpp \
+ C:/VRack/Rack-SDK/dep/include/speex/speex_resampler.h \
+ C:/VRack/Rack-SDK/dep/include/speex/speexdsp_types.h \
+ C:/VRack/Rack-SDK/include/dsp/fir.hpp \
+ C:/VRack/Rack-SDK/include/dsp/approx.hpp \
+ C:/VRack/Rack-SDK/include/dsp/midi.hpp \
+ C:/VRack/Rack-SDK/include/dsp/vumeter.hpp \
+ C:/VRack/Rack-SDK/include/dsp/filter.hpp \
+ C:/VRack/Rack-SDK/include/dsp/digital.hpp \
+ C:/VRack/Rack-SDK/include/dsp/convert.hpp
+src/plugin.hpp:
+C:/VRack/Rack-SDK/include/rack.hpp:
+C:/VRack/Rack-SDK/include/common.hpp:
+C:/VRack/Rack-SDK/include/arch.hpp:
+C:/VRack/Rack-SDK/include/logger.hpp:
+C:/VRack/Rack-SDK/include/math.hpp:
+C:/VRack/Rack-SDK/include/string.hpp:
+C:/VRack/Rack-SDK/include/system.hpp:
+C:/VRack/Rack-SDK/include/mutex.hpp:
+C:/VRack/Rack-SDK/include/random.hpp:
+C:/VRack/Rack-SDK/include/network.hpp:
+C:/VRack/Rack-SDK/dep/include/jansson.h:
+C:/VRack/Rack-SDK/dep/include/jansson_config.h:
+C:/VRack/Rack-SDK/include/asset.hpp:
+C:/VRack/Rack-SDK/include/window/Window.hpp:
+C:/VRack/Rack-SDK/include/window/Svg.hpp:
+C:/VRack/Rack-SDK/dep/include/nanovg.h:
+C:/VRack/Rack-SDK/dep/include/nanosvg.h:
+C:/VRack/Rack-SDK/dep/include/GL/glew.h:
+C:/VRack/Rack-SDK/dep/include/GLFW/glfw3.h:
+C:/VRack/Rack-SDK/dep/include/nanovg_gl.h:
+C:/VRack/Rack-SDK/dep/include/nanovg_gl_utils.h:
+C:/VRack/Rack-SDK/include/context.hpp:
+C:/VRack/Rack-SDK/include/audio.hpp:
+C:/VRack/Rack-SDK/include/midi.hpp:
+C:/VRack/Rack-SDK/include/settings.hpp:
+C:/VRack/Rack-SDK/include/color.hpp:
+C:/VRack/Rack-SDK/include/helpers.hpp:
+C:/VRack/Rack-SDK/include/plugin/Model.hpp:
+C:/VRack/Rack-SDK/include/plugin/Plugin.hpp:
+C:/VRack/Rack-SDK/include/ui/MenuOverlay.hpp:
+C:/VRack/Rack-SDK/include/widget/OpaqueWidget.hpp:
+C:/VRack/Rack-SDK/include/widget/Widget.hpp:
+C:/VRack/Rack-SDK/include/widget/event.hpp:
+C:/VRack/Rack-SDK/include/weakptr.hpp:
+C:/VRack/Rack-SDK/include/ui/common.hpp:
+C:/VRack/Rack-SDK/dep/include/blendish.h:
+C:/VRack/Rack-SDK/include/ui/MenuItem.hpp:
+C:/VRack/Rack-SDK/include/ui/Menu.hpp:
+C:/VRack/Rack-SDK/include/ui/MenuEntry.hpp:
+C:/VRack/Rack-SDK/include/ui/MenuLabel.hpp:
+C:/VRack/Rack-SDK/include/app/PortWidget.hpp:
+C:/VRack/Rack-SDK/include/app/common.hpp:
+C:/VRack/Rack-SDK/include/ui/Tooltip.hpp:
+C:/VRack/Rack-SDK/include/engine/Module.hpp:
+C:/VRack/Rack-SDK/include/engine/Param.hpp:
+C:/VRack/Rack-SDK/include/engine/Port.hpp:
+C:/VRack/Rack-SDK/include/engine/Light.hpp:
+C:/VRack/Rack-SDK/include/engine/ParamQuantity.hpp:
+C:/VRack/Rack-SDK/include/Quantity.hpp:
+C:/VRack/Rack-SDK/include/engine/PortInfo.hpp:
+C:/VRack/Rack-SDK/include/engine/LightInfo.hpp:
+C:/VRack/Rack-SDK/include/app/ParamWidget.hpp:
+C:/VRack/Rack-SDK/include/app/ModuleLightWidget.hpp:
+C:/VRack/Rack-SDK/include/app/MultiLightWidget.hpp:
+C:/VRack/Rack-SDK/include/app/LightWidget.hpp:
+C:/VRack/Rack-SDK/include/widget/TransparentWidget.hpp:
+C:/VRack/Rack-SDK/include/app/Scene.hpp:
+C:/VRack/Rack-SDK/include/app/RackScrollWidget.hpp:
+C:/VRack/Rack-SDK/include/ui/ScrollWidget.hpp:
+C:/VRack/Rack-SDK/include/ui/Scrollbar.hpp:
+C:/VRack/Rack-SDK/include/widget/ZoomWidget.hpp:
+C:/VRack/Rack-SDK/include/app/RackWidget.hpp:
+C:/VRack/Rack-SDK/include/widget/FramebufferWidget.hpp:
+C:/VRack/Rack-SDK/include/app/RailWidget.hpp:
+C:/VRack/Rack-SDK/include/app/ModuleWidget.hpp:
+C:/VRack/Rack-SDK/include/history.hpp:
+C:/VRack/Rack-SDK/include/app/CableWidget.hpp:
+C:/VRack/Rack-SDK/include/engine/Cable.hpp:
+C:/VRack/Rack-SDK/include/app/SvgPanel.hpp:
+C:/VRack/Rack-SDK/include/widget/SvgWidget.hpp:
+C:/VRack/Rack-SDK/include/componentlibrary.hpp:
+C:/VRack/Rack-SDK/include/app/SvgKnob.hpp:
+C:/VRack/Rack-SDK/include/app/Knob.hpp:
+C:/VRack/Rack-SDK/include/widget/TransformWidget.hpp:
+C:/VRack/Rack-SDK/include/app/CircularShadow.hpp:
+C:/VRack/Rack-SDK/include/app/SvgSlider.hpp:
+C:/VRack/Rack-SDK/include/app/SliderKnob.hpp:
+C:/VRack/Rack-SDK/include/app/SvgPort.hpp:
+C:/VRack/Rack-SDK/include/app/SvgSwitch.hpp:
+C:/VRack/Rack-SDK/include/app/Switch.hpp:
+C:/VRack/Rack-SDK/include/app/SvgScrew.hpp:
+C:/VRack/Rack-SDK/include/app/AudioDisplay.hpp:
+C:/VRack/Rack-SDK/include/app/LedDisplay.hpp:
+C:/VRack/Rack-SDK/include/ui/TextField.hpp:
+C:/VRack/Rack-SDK/include/app/SvgButton.hpp:
+C:/VRack/Rack-SDK/include/app/MidiDisplay.hpp:
+C:/VRack/Rack-SDK/include/widget/OpenGlWidget.hpp:
+C:/VRack/Rack-SDK/include/ui/List.hpp:
+C:/VRack/Rack-SDK/include/ui/TooltipOverlay.hpp:
+C:/VRack/Rack-SDK/include/ui/Slider.hpp:
+C:/VRack/Rack-SDK/include/ui/ProgressBar.hpp:
+C:/VRack/Rack-SDK/include/ui/MenuSeparator.hpp:
+C:/VRack/Rack-SDK/include/ui/Label.hpp:
+C:/VRack/Rack-SDK/include/ui/SequentialLayout.hpp:
+C:/VRack/Rack-SDK/include/ui/Button.hpp:
+C:/VRack/Rack-SDK/include/ui/ChoiceButton.hpp:
+C:/VRack/Rack-SDK/include/ui/OptionButton.hpp:
+C:/VRack/Rack-SDK/include/ui/RadioButton.hpp:
+C:/VRack/Rack-SDK/include/engine/ParamHandle.hpp:
+C:/VRack/Rack-SDK/include/engine/Engine.hpp:
+C:/VRack/Rack-SDK/include/plugin.hpp:
+C:/VRack/Rack-SDK/include/plugin/callbacks.hpp:
+C:/VRack/Rack-SDK/include/dsp/common.hpp:
+C:/VRack/Rack-SDK/include/simd/functions.hpp:
+C:/VRack/Rack-SDK/include/simd/Vector.hpp:
+C:/VRack/Rack-SDK/include/simd/common.hpp:
+C:/VRack/Rack-SDK/include/simd/sse_mathfun_extension.h:
+C:/VRack/Rack-SDK/include/simd/sse_mathfun.h:
+C:/VRack/Rack-SDK/include/dsp/window.hpp:
+C:/VRack/Rack-SDK/include/dsp/ode.hpp:
+C:/VRack/Rack-SDK/include/dsp/minblep.hpp:
+C:/VRack/Rack-SDK/include/dsp/fft.hpp:
+C:/VRack/Rack-SDK/dep/include/pffft.h:
+C:/VRack/Rack-SDK/include/dsp/ringbuffer.hpp:
+C:/VRack/Rack-SDK/include/dsp/resampler.hpp:
+C:/VRack/Rack-SDK/dep/include/speex/speex_resampler.h:
+C:/VRack/Rack-SDK/dep/include/speex/speexdsp_types.h:
+C:/VRack/Rack-SDK/include/dsp/fir.hpp:
+C:/VRack/Rack-SDK/include/dsp/approx.hpp:
+C:/VRack/Rack-SDK/include/dsp/midi.hpp:
+C:/VRack/Rack-SDK/include/dsp/vumeter.hpp:
+C:/VRack/Rack-SDK/include/dsp/filter.hpp:
+C:/VRack/Rack-SDK/include/dsp/digital.hpp:
+C:/VRack/Rack-SDK/include/dsp/convert.hpp:
---
+++ build/src/OctIFer.cpp.d
@@ -0,0 +1,245 @@
+build/src/OctIFer.cpp.o: src/OctIFer.cpp src/plugin.hpp \
+ c:/VRack/Rack-SDK/include/rack.hpp c:/VRack/Rack-SDK/include/common.hpp \
+ c:/VRack/Rack-SDK/include/arch.hpp c:/VRack/Rack-SDK/include/logger.hpp \
+ c:/VRack/Rack-SDK/include/math.hpp c:/VRack/Rack-SDK/include/string.hpp \
+ c:/VRack/Rack-SDK/include/system.hpp c:/VRack/Rack-SDK/include/mutex.hpp \
+ c:/VRack/Rack-SDK/include/random.hpp \
+ c:/VRack/Rack-SDK/include/network.hpp \
+ c:/VRack/Rack-SDK/dep/include/jansson.h \
+ c:/VRack/Rack-SDK/dep/include/jansson_config.h \
+ c:/VRack/Rack-SDK/include/asset.hpp \
+ c:/VRack/Rack-SDK/include/window/Window.hpp \
+ c:/VRack/Rack-SDK/include/window/Svg.hpp \
+ c:/VRack/Rack-SDK/dep/include/nanovg.h \
+ c:/VRack/Rack-SDK/dep/include/nanosvg.h \
+ c:/VRack/Rack-SDK/dep/include/GL/glew.h \
+ c:/VRack/Rack-SDK/dep/include/GLFW/glfw3.h \
+ c:/VRack/Rack-SDK/dep/include/nanovg_gl.h \
+ c:/VRack/Rack-SDK/dep/include/nanovg_gl_utils.h \
+ c:/VRack/Rack-SDK/include/context.hpp \
+ c:/VRack/Rack-SDK/include/audio.hpp c:/VRack/Rack-SDK/include/midi.hpp \
+ c:/VRack/Rack-SDK/include/settings.hpp \
+ c:/VRack/Rack-SDK/include/color.hpp \
+ c:/VRack/Rack-SDK/include/helpers.hpp \
+ c:/VRack/Rack-SDK/include/plugin/Model.hpp \
+ c:/VRack/Rack-SDK/include/plugin/Plugin.hpp \
+ c:/VRack/Rack-SDK/include/ui/MenuOverlay.hpp \
+ c:/VRack/Rack-SDK/include/widget/OpaqueWidget.hpp \
+ c:/VRack/Rack-SDK/include/widget/Widget.hpp \
+ c:/VRack/Rack-SDK/include/widget/event.hpp \
+ c:/VRack/Rack-SDK/include/weakptr.hpp \
+ c:/VRack/Rack-SDK/include/ui/common.hpp \
+ c:/VRack/Rack-SDK/dep/include/blendish.h \
+ c:/VRack/Rack-SDK/include/ui/MenuItem.hpp \
+ c:/VRack/Rack-SDK/include/ui/Menu.hpp \
+ c:/VRack/Rack-SDK/include/ui/MenuEntry.hpp \
+ c:/VRack/Rack-SDK/include/ui/MenuLabel.hpp \
+ c:/VRack/Rack-SDK/include/app/PortWidget.hpp \
+ c:/VRack/Rack-SDK/include/app/common.hpp \
+ c:/VRack/Rack-SDK/include/ui/Tooltip.hpp \
+ c:/VRack/Rack-SDK/include/engine/Module.hpp \
+ c:/VRack/Rack-SDK/include/engine/Param.hpp \
+ c:/VRack/Rack-SDK/include/engine/Port.hpp \
+ c:/VRack/Rack-SDK/include/engine/Light.hpp \
+ c:/VRack/Rack-SDK/include/engine/ParamQuantity.hpp \
+ c:/VRack/Rack-SDK/include/Quantity.hpp \
+ c:/VRack/Rack-SDK/include/engine/PortInfo.hpp \
+ c:/VRack/Rack-SDK/include/engine/LightInfo.hpp \
+ c:/VRack/Rack-SDK/include/app/ParamWidget.hpp \
+ c:/VRack/Rack-SDK/include/app/ModuleLightWidget.hpp \
+ c:/VRack/Rack-SDK/include/app/MultiLightWidget.hpp \
+ c:/VRack/Rack-SDK/include/app/LightWidget.hpp \
+ c:/VRack/Rack-SDK/include/widget/TransparentWidget.hpp \
+ c:/VRack/Rack-SDK/include/app/Scene.hpp \
+ c:/VRack/Rack-SDK/include/app/RackScrollWidget.hpp \
+ c:/VRack/Rack-SDK/include/ui/ScrollWidget.hpp \
+ c:/VRack/Rack-SDK/include/ui/Scrollbar.hpp \
+ c:/VRack/Rack-SDK/include/widget/ZoomWidget.hpp \
+ c:/VRack/Rack-SDK/include/app/RackWidget.hpp \
+ c:/VRack/Rack-SDK/include/widget/FramebufferWidget.hpp \
+ c:/VRack/Rack-SDK/include/app/RailWidget.hpp \
+ c:/VRack/Rack-SDK/include/app/ModuleWidget.hpp \
+ c:/VRack/Rack-SDK/include/history.hpp \
+ c:/VRack/Rack-SDK/include/app/CableWidget.hpp \
+ c:/VRack/Rack-SDK/include/engine/Cable.hpp \
+ c:/VRack/Rack-SDK/include/app/SvgPanel.hpp \
+ c:/VRack/Rack-SDK/include/widget/SvgWidget.hpp \
+ c:/VRack/Rack-SDK/include/componentlibrary.hpp \
+ c:/VRack/Rack-SDK/include/app/SvgKnob.hpp \
+ c:/VRack/Rack-SDK/include/app/Knob.hpp \
+ c:/VRack/Rack-SDK/include/widget/TransformWidget.hpp \
+ c:/VRack/Rack-SDK/include/app/CircularShadow.hpp \
+ c:/VRack/Rack-SDK/include/app/SvgSlider.hpp \
+ c:/VRack/Rack-SDK/include/app/SliderKnob.hpp \
+ c:/VRack/Rack-SDK/include/app/SvgPort.hpp \
+ c:/VRack/Rack-SDK/include/app/SvgSwitch.hpp \
+ c:/VRack/Rack-SDK/include/app/Switch.hpp \
+ c:/VRack/Rack-SDK/include/app/SvgScrew.hpp \
+ c:/VRack/Rack-SDK/include/app/AudioDisplay.hpp \
+ c:/VRack/Rack-SDK/include/app/LedDisplay.hpp \
+ c:/VRack/Rack-SDK/include/ui/TextField.hpp \
+ c:/VRack/Rack-SDK/include/app/SvgButton.hpp \
+ c:/VRack/Rack-SDK/include/app/MidiDisplay.hpp \
+ c:/VRack/Rack-SDK/include/widget/OpenGlWidget.hpp \
+ c:/VRack/Rack-SDK/include/ui/List.hpp \
+ c:/VRack/Rack-SDK/include/ui/TooltipOverlay.hpp \
+ c:/VRack/Rack-SDK/include/ui/Slider.hpp \
+ c:/VRack/Rack-SDK/include/ui/ProgressBar.hpp \
+ c:/VRack/Rack-SDK/include/ui/MenuSeparator.hpp \
+ c:/VRack/Rack-SDK/include/ui/Label.hpp \
+ c:/VRack/Rack-SDK/include/ui/SequentialLayout.hpp \
+ c:/VRack/Rack-SDK/include/ui/Button.hpp \
+ c:/VRack/Rack-SDK/include/ui/ChoiceButton.hpp \
+ c:/VRack/Rack-SDK/include/ui/OptionButton.hpp \
+ c:/VRack/Rack-SDK/include/ui/RadioButton.hpp \
+ c:/VRack/Rack-SDK/include/engine/ParamHandle.hpp \
+ c:/VRack/Rack-SDK/include/engine/Engine.hpp \
+ c:/VRack/Rack-SDK/include/plugin.hpp \
+ c:/VRack/Rack-SDK/include/plugin/callbacks.hpp \
+ c:/VRack/Rack-SDK/include/dsp/common.hpp \
+ c:/VRack/Rack-SDK/include/simd/functions.hpp \
+ c:/VRack/Rack-SDK/include/simd/Vector.hpp \
+ c:/VRack/Rack-SDK/include/simd/common.hpp \
+ c:/VRack/Rack-SDK/include/simd/sse_mathfun_extension.h \
+ c:/VRack/Rack-SDK/include/simd/sse_mathfun.h \
+ c:/VRack/Rack-SDK/include/dsp/window.hpp \
+ c:/VRack/Rack-SDK/include/dsp/ode.hpp \
+ c:/VRack/Rack-SDK/include/dsp/minblep.hpp \
+ c:/VRack/Rack-SDK/include/dsp/fft.hpp \
+ c:/VRack/Rack-SDK/dep/include/pffft.h \
+ c:/VRack/Rack-SDK/include/dsp/ringbuffer.hpp \
+ c:/VRack/Rack-SDK/include/dsp/resampler.hpp \
+ c:/VRack/Rack-SDK/dep/include/speex/speex_resampler.h \
+ c:/VRack/Rack-SDK/dep/include/speex/speexdsp_types.h \
+ c:/VRack/Rack-SDK/include/dsp/fir.hpp \
+ c:/VRack/Rack-SDK/include/dsp/approx.hpp \
+ c:/VRack/Rack-SDK/include/dsp/midi.hpp \
+ c:/VRack/Rack-SDK/include/dsp/vumeter.hpp \
+ c:/VRack/Rack-SDK/include/dsp/filter.hpp \
+ c:/VRack/Rack-SDK/include/dsp/digital.hpp \
+ c:/VRack/Rack-SDK/include/dsp/convert.hpp
+src/plugin.hpp:
+c:/VRack/Rack-SDK/include/rack.hpp:
+c:/VRack/Rack-SDK/include/common.hpp:
+c:/VRack/Rack-SDK/include/arch.hpp:
+c:/VRack/Rack-SDK/include/logger.hpp:
+c:/VRack/Rack-SDK/include/math.hpp:
+c:/VRack/Rack-SDK/include/string.hpp:
+c:/VRack/Rack-SDK/include/system.hpp:
+c:/VRack/Rack-SDK/include/mutex.hpp:
+c:/VRack/Rack-SDK/include/random.hpp:
+c:/VRack/Rack-SDK/include/network.hpp:
+c:/VRack/Rack-SDK/dep/include/jansson.h:
+c:/VRack/Rack-SDK/dep/include/jansson_config.h:
+c:/VRack/Rack-SDK/include/asset.hpp:
+c:/VRack/Rack-SDK/include/window/Window.hpp:
+c:/VRack/Rack-SDK/include/window/Svg.hpp:
+c:/VRack/Rack-SDK/dep/include/nanovg.h:
+c:/VRack/Rack-SDK/dep/include/nanosvg.h:
+c:/VRack/Rack-SDK/dep/include/GL/glew.h:
+c:/VRack/Rack-SDK/dep/include/GLFW/glfw3.h:
+c:/VRack/Rack-SDK/dep/include/nanovg_gl.h:
+c:/VRack/Rack-SDK/dep/include/nanovg_gl_utils.h:
+c:/VRack/Rack-SDK/include/context.hpp:
+c:/VRack/Rack-SDK/include/audio.hpp:
+c:/VRack/Rack-SDK/include/midi.hpp:
+c:/VRack/Rack-SDK/include/settings.hpp:
+c:/VRack/Rack-SDK/include/color.hpp:
+c:/VRack/Rack-SDK/include/helpers.hpp:
+c:/VRack/Rack-SDK/include/plugin/Model.hpp:
+c:/VRack/Rack-SDK/include/plugin/Plugin.hpp:
+c:/VRack/Rack-SDK/include/ui/MenuOverlay.hpp:
+c:/VRack/Rack-SDK/include/widget/OpaqueWidget.hpp:
+c:/VRack/Rack-SDK/include/widget/Widget.hpp:
+c:/VRack/Rack-SDK/include/widget/event.hpp:
+c:/VRack/Rack-SDK/include/weakptr.hpp:
+c:/VRack/Rack-SDK/include/ui/common.hpp:
+c:/VRack/Rack-SDK/dep/include/blendish.h:
+c:/VRack/Rack-SDK/include/ui/MenuItem.hpp:
+c:/VRack/Rack-SDK/include/ui/Menu.hpp:
+c:/VRack/Rack-SDK/include/ui/MenuEntry.hpp:
+c:/VRack/Rack-SDK/include/ui/MenuLabel.hpp:
+c:/VRack/Rack-SDK/include/app/PortWidget.hpp:
+c:/VRack/Rack-SDK/include/app/common.hpp:
+c:/VRack/Rack-SDK/include/ui/Tooltip.hpp:
+c:/VRack/Rack-SDK/include/engine/Module.hpp:
+c:/VRack/Rack-SDK/include/engine/Param.hpp:
+c:/VRack/Rack-SDK/include/engine/Port.hpp:
+c:/VRack/Rack-SDK/include/engine/Light.hpp:
+c:/VRack/Rack-SDK/include/engine/ParamQuantity.hpp:
+c:/VRack/Rack-SDK/include/Quantity.hpp:
+c:/VRack/Rack-SDK/include/engine/PortInfo.hpp:
+c:/VRack/Rack-SDK/include/engine/LightInfo.hpp:
+c:/VRack/Rack-SDK/include/app/ParamWidget.hpp:
+c:/VRack/Rack-SDK/include/app/ModuleLightWidget.hpp:
+c:/VRack/Rack-SDK/include/app/MultiLightWidget.hpp:
+c:/VRack/Rack-SDK/include/app/LightWidget.hpp:
+c:/VRack/Rack-SDK/include/widget/TransparentWidget.hpp:
+c:/VRack/Rack-SDK/include/app/Scene.hpp:
+c:/VRack/Rack-SDK/include/app/RackScrollWidget.hpp:
+c:/VRack/Rack-SDK/include/ui/ScrollWidget.hpp:
+c:/VRack/Rack-SDK/include/ui/Scrollbar.hpp:
+c:/VRack/Rack-SDK/include/widget/ZoomWidget.hpp:
+c:/VRack/Rack-SDK/include/app/RackWidget.hpp:
+c:/VRack/Rack-SDK/include/widget/FramebufferWidget.hpp:
+c:/VRack/Rack-SDK/include/app/RailWidget.hpp:
+c:/VRack/Rack-SDK/include/app/ModuleWidget.hpp:
+c:/VRack/Rack-SDK/include/history.hpp:
+c:/VRack/Rack-SDK/include/app/CableWidget.hpp:
+c:/VRack/Rack-SDK/include/engine/Cable.hpp:
+c:/VRack/Rack-SDK/include/app/SvgPanel.hpp:
+c:/VRack/Rack-SDK/include/widget/SvgWidget.hpp:
+c:/VRack/Rack-SDK/include/componentlibrary.hpp:
+c:/VRack/Rack-SDK/include/app/SvgKnob.hpp:
+c:/VRack/Rack-SDK/include/app/Knob.hpp:
+c:/VRack/Rack-SDK/include/widget/TransformWidget.hpp:
+c:/VRack/Rack-SDK/include/app/CircularShadow.hpp:
+c:/VRack/Rack-SDK/include/app/SvgSlider.hpp:
+c:/VRack/Rack-SDK/include/app/SliderKnob.hpp:
+c:/VRack/Rack-SDK/include/app/SvgPort.hpp:
+c:/VRack/Rack-SDK/include/app/SvgSwitch.hpp:
+c:/VRack/Rack-SDK/include/app/Switch.hpp:
+c:/VRack/Rack-SDK/include/app/SvgScrew.hpp:
+c:/VRack/Rack-SDK/include/app/AudioDisplay.hpp:
+c:/VRack/Rack-SDK/include/app/LedDisplay.hpp:
+c:/VRack/Rack-SDK/include/ui/TextField.hpp:
+c:/VRack/Rack-SDK/include/app/SvgButton.hpp:
+c:/VRack/Rack-SDK/include/app/MidiDisplay.hpp:
+c:/VRack/Rack-SDK/include/widget/OpenGlWidget.hpp:
+c:/VRack/Rack-SDK/include/ui/List.hpp:
+c:/VRack/Rack-SDK/include/ui/TooltipOverlay.hpp:
+c:/VRack/Rack-SDK/include/ui/Slider.hpp:
+c:/VRack/Rack-SDK/include/ui/ProgressBar.hpp:
+c:/VRack/Rack-SDK/include/ui/MenuSeparator.hpp:
+c:/VRack/Rack-SDK/include/ui/Label.hpp:
+c:/VRack/Rack-SDK/include/ui/SequentialLayout.hpp:
+c:/VRack/Rack-SDK/include/ui/Button.hpp:
+c:/VRack/Rack-SDK/include/ui/ChoiceButton.hpp:
+c:/VRack/Rack-SDK/include/ui/OptionButton.hpp:
+c:/VRack/Rack-SDK/include/ui/RadioButton.hpp:
+c:/VRack/Rack-SDK/include/engine/ParamHandle.hpp:
+c:/VRack/Rack-SDK/include/engine/Engine.hpp:
+c:/VRack/Rack-SDK/include/plugin.hpp:
+c:/VRack/Rack-SDK/include/plugin/callbacks.hpp:
+c:/VRack/Rack-SDK/include/dsp/common.hpp:
+c:/VRack/Rack-SDK/include/simd/functions.hpp:
+c:/VRack/Rack-SDK/include/simd/Vector.hpp:
+c:/VRack/Rack-SDK/include/simd/common.hpp:
+c:/VRack/Rack-SDK/include/simd/sse_mathfun_extension.h:
+c:/VRack/Rack-SDK/include/simd/sse_mathfun.h:
+c:/VRack/Rack-SDK/include/dsp/window.hpp:
+c:/VRack/Rack-SDK/include/dsp/ode.hpp:
+c:/VRack/Rack-SDK/include/dsp/minblep.hpp:
+c:/VRack/Rack-SDK/include/dsp/fft.hpp:
+c:/VRack/Rack-SDK/dep/include/pffft.h:
+c:/VRack/Rack-SDK/include/dsp/ringbuffer.hpp:
+c:/VRack/Rack-SDK/include/dsp/resampler.hpp:
+c:/VRack/Rack-SDK/dep/include/speex/speex_resampler.h:
+c:/VRack/Rack-SDK/dep/include/speex/speexdsp_types.h:
+c:/VRack/Rack-SDK/include/dsp/fir.hpp:
+c:/VRack/Rack-SDK/include/dsp/approx.hpp:
+c:/VRack/Rack-SDK/include/dsp/midi.hpp:
+c:/VRack/Rack-SDK/include/dsp/vumeter.hpp:
+c:/VRack/Rack-SDK/include/dsp/filter.hpp:
+c:/VRack/Rack-SDK/include/dsp/digital.hpp:
+c:/VRack/Rack-SDK/include/dsp/convert.hpp:
